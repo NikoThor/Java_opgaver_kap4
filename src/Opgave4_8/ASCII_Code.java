4dc574f409d71782e8c0160e9258038599ca932c
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-10-20 17:31:29 +0200

    Fixet stavefejl og lavet en shout til første input

--- src/Opgave4_8/ASCII_Code.java
+++ src/Opgave4_8/ASCII_Code.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class ASCII_Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // initialize new scanner object
+        System.out.println("Please enter a character");
         int i = sc.next().charAt(0); // Takes a char, then converts it into the ASCII CODE
-        System.out.println(i); // Shouts out the ASCII code
+        System.out.println("The ASCII code for the character is "+ "" + i); // Shouts out the ASCII code
     }
 }
