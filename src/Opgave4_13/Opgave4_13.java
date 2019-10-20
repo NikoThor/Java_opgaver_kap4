101c65c066afe9b430880859f55ea053eed8fc3b
Nikolaj Thor Christensen <54975617+NikoThor@users.noreply.github.com>
2019-10-20 17:58:38 +0200

    Lavet 4.13

--- src/Opgave4_13/Opgave4_13.java
+++ src/Opgave4_13/Opgave4_13.java
@@ -10,10 +10,10 @@ public class Opgave4_13 {
         char ch = a.charAt(0);
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
         {
-            System.out.println("Entered character "+ch+" is  Vowel");
+            System.out.println("Entered character "+ch+" is a vowel");
         }
         else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-            System.out.println("Entered character "+ch+" is Consonant");
+            System.out.println("Entered character "+ch+" is a consonant");
         else
             System.out.println("Not an alphabet");
     }
